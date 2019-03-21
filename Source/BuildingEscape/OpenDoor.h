7a82a433d023348e519865e1e7a7ed4108227ba2
Juri <23640912+jthommen@users.noreply.github.com>
2019-03-21 19:34:21 +0000

    Add materials, create pressure plate

--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -4,25 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
+private:
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+		float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate = 0;
 
 public:	
 	// Sets default values for this component's properties
 	UOpenDoor();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
 };
